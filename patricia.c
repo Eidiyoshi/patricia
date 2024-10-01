70861044cc25641e1c09436ca7ea9043a43dfc44
Eidiyoshi <eidiyoshi@hotmail.com>
2024-10-01 18:38:11 -0300

    print working, some fixes, removed not included yet

--- patricia.c
+++ patricia.c
@@ -2,12 +2,13 @@
 #include <string.h>
 #include <limits.h>
 #include <stdlib.h>
+#include <math.h>
 
 int tamanho;
 
 typedef struct patricia_node {
     int bit; // Posição do bit que discrimina este nó
-    unsigned chave; // Chave armazenada no nó, NULL para nós intermediários // ? como assim nos intermediario
+    unsigned chave; // Chave armazenada no nó, NULL para nós intermediários // ? como assim nós intermediario
     struct patricia_node *esq; // Subárvore esquerda
     struct patricia_node *dir; // Subárvore direita
 } PatriciaNode;
@@ -16,17 +17,15 @@ unsigned bit(unsigned chave, int k) {
     return chave >> ( tamanho - 1 - k) & 1;
 }
 
-PatriciaNode *busca_rec(PatriciaNode *arvore, unsigned x, int nivel) {
-    if (arvore == NULL){
-        return NULL;
+PatriciaNode *busca_rec(PatriciaNode *arvore, unsigned x, int w) {
+    if (arvore->bit <= w){
+        return arvore;
     }
-    if (x == arvore->chave){
-    return arvore;
+    if (bit(x, arvore->bit) == 0){
+        return busca_rec(arvore->esq, x, arvore->bit);
     }
-    if (bit(x, nivel) == 0){
-        return busca_rec(arvore->esq, x, nivel+1);
-    }else{
-        return busca_rec(arvore->dir, x, nivel+1);
+    else{
+        return busca_rec(arvore->dir, x, arvore->bit);
     }
 }
 
@@ -61,18 +60,12 @@ PatriciaNode* inicializa(){
     arvore->esq = arvore->dir = arvore;
     arvore->bit = -1;
 
-    printf("Digite a primeira chave: ");
-    unsigned chave;
-    fflush(stdin);
-    scanf("%u",&chave);
-
-    arvore->esq = insere_rec(arvore, chave, 0, arvore);
-
     return arvore;
 }
 
 void insere(PatriciaNode *arvore, unsigned chave) {
     int i;
+
     PatriciaNode *t = busca_rec(arvore->esq, chave, -1);
 
     if (chave == t->chave){
@@ -83,10 +76,68 @@ void insere(PatriciaNode *arvore, unsigned chave) {
     arvore->esq = insere_rec(arvore->esq, chave, i, arvore);
 }
 
-PatriciaNode * busca(PatriciaNode *arvore, unsigned x) {
-    return busca_rec(arvore, x, 0);
+PatriciaNode *busca(PatriciaNode *arvore, unsigned x) {
+    PatriciaNode *t = busca_rec(arvore->esq, x, -1);
+    return t->chave == x ? t : NULL; // se t->chave == x, retorna t, caso falso null
 }
 
+void printar_info(PatriciaNode *raiz){
+    printf("  \n");
+    if( raiz->bit != -1 ){ // criacao de espacos
+        for(int i = 0; i <= raiz->bit; i++){
+            printf("     ");
+        }
+    }
+
+
+    char buffer[tamanho];
+    itoa(raiz->chave, buffer, 2); // transformacao da chave pra binario
+
+    char zeros[tamanho];
+    strcpy(zeros,"");
+
+    int contador = tamanho, modificador = strlen(buffer);
+    while( ( contador - modificador ) > 0 ){
+        strcat(zeros, "0");
+        modificador++;
+    }
+
+    strcat(zeros,buffer);
+
+    if(raiz->bit == -1){
+        printf("Bit: -1, Chave:MAX_INT\n");
+    }else{
+        printf("Bit: %d, Chave: %s\n", raiz->bit, zeros );
+    }
+}
+
+void pegar_rec(PatriciaNode *raiz){
+
+    if(raiz == NULL){
+        return;
+    }
+
+    if( raiz->esq->bit > raiz->bit ){
+        for(int i = 0; i <= raiz->bit; i++){
+            printf("      ");
+        }
+        pegar_rec(raiz->esq);
+    }else{
+        printf("\n");
+    }
+
+    printar_info(raiz);
+
+    if( raiz->dir->bit > raiz->bit){
+
+        for(int i = 0; i <= raiz->bit; i++){
+            printf("      ");
+        }
+        pegar_rec(raiz->dir);
+    }else{
+        printf("\n");
+    }
+}
 
 void main(){
     printf("Bem vindo trabalho arvore Patricia\n");
@@ -100,12 +151,12 @@ void main(){
 
     int escolha;
     do{
-        printf("1- Inserir chave\n");
+        printf("\n\n1- Inserir chave\n");
         printf("2- Buscar chave\n");
         printf("3- Printar arvore\n");
         printf("4- Remover ( nao funcionando )\n");
 
-        printf("99- Sair para sempre desse mundo\n\n");
+        printf("99- Sair\n\n");
 
 
         fflush(stdin);
@@ -114,25 +165,32 @@ void main(){
 
         switch(escolha){
             case 1:
-                printf("Qual valor deseja inserir?\n");
+                printf("Valor que deseja inserir: ");
 
                 scanf("%u",&chave);
 
-                insere(raiz,chave);
+                if( pow(2,tamanho) - 1 <= chave  ){
+                    printf("\nATENCAO!!! VALOR EXCEDIDO, POR FAVOR DIGITE OUTRO !!!\n");
+                }else{
+                    insere(raiz,chave);
+                    printf("Valor inserido com sucesso");
+                }
                 break;
 
             case 2:
-                printf("Qual valor deseja buscar?\n");
+                printf("Qual valor deseja buscar: ");
 
                 scanf("%u",&chave);
 
                 if( busca(raiz,chave) != NULL ){
-
+                    printf("\nEla existe\n");
+                }else{
+                    printf("\nNao existe\n");
                 }
                 break;
 
             case 3:
-                //printar;
+                pegar_rec(raiz);
                 break;
 
             case 4:
